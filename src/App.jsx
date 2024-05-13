3fe0a571d9f3cac0af9ef7f55e331d4b3f250251
Kabasiita-Victoria <vkabasiita03@gmail.com>
2024-05-14 00:40:44 +0300

    add routing with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home";
+
 function App(){
   return(
-    <div>
-      <h1 className="font-mono text-amber-500 text-3xl">Hello World</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="m-10">
+      <h1 className="font-mono text-yellow-400 text-5xl">Health</h1>
+      <p className="text-2x1">this is care</p>
+    </div>
+  );
+}
+
+export default Home;
